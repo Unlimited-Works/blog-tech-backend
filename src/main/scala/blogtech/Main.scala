5b2c8bd53fc4767096f967eb04c37318b4e26c51
LoranceChen <w68055010@qq.com>
2018-01-07 19:43:10 +0800

    refine

--- src/main/scala/blogtech/Main.scala
+++ src/main/scala/blogtech/Main.scala
@@ -1,17 +1,24 @@
 package blogtech
 
-import io.circe.Json
-import org.http4s.HttpService
 import cats.effect.IO
-import org.http4s.circe._
-import org.http4s.dsl.Http4sDsl
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.util.StreamApp
 
 /**
   *
   */
-object Main extends StreamApp[IO] with Http4sDsl[IO] {
+object Main extends StreamApp[IO] {
+  /**
+    * load config
+    */
+
+  /**
+    * check and init environment:
+    *   - git repos path
+    *
+    */
+
+  //get all service
   val services = blogtech.http.Http.httpService
 
   def stream(args: List[String], requestShutdown: IO[Unit]) =
