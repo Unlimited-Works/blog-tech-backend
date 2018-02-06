491e512684265100475f5473afa363ce9bda62fc
LoranceChen <w68055010@qq.com>
2018-02-06 18:29:08 +0800

    ready login page

--- src/main/scala/blogtech/http/Login.scala
+++ src/main/scala/blogtech/http/Login.scala
@@ -1,16 +1,23 @@
 package blogtech.http
 
-import org.http4s.HttpService
+import org.http4s.{Header, HttpService}
 import cats.effect.IO
 import org.http4s.dsl.Http4sDsl
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
 
 /**
   * login and register API
   */
 object Login extends Http4sDsl[IO] with Service {
+
   override val service: HttpService[IO] = HttpService[IO] {
-    case GET -> Root / "hello" / name =>
-//      Ok(Json.obj("message" -> Json.fromString(s"Hello, $name")))
-      Ok("ok")
+    case GET -> Root  =>
+      //return login page
+      Ok("login page")
+        .putHeaders(Header("Content-Type", "text/html"))
+    case POST -> Root / "login.json" =>
+      Ok(compact(render("status" -> 200)))
+
   }
 }
