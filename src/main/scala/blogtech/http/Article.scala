5a69646ec85bb9afb6f88838c3dd7468f61451ae
LoranceChen <w68055010@qq.com>
2018-02-12 22:48:47 +0800

    article json api

--- src/main/scala/blogtech/http/Article.scala
+++ src/main/scala/blogtech/http/Article.scala
@@ -5,6 +5,8 @@ import monix.eval.Task
 import org.http4s.dsl.Http4sDsl
 import org.http4s.{Header, HttpService}
 import monix.execution.Scheduler.Implicits.global
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
 
 /**
   * http://{host}/{username}
@@ -12,6 +14,26 @@ import monix.execution.Scheduler.Implicits.global
 object Article extends Http4sDsl[IO] with Service {
   override val service: HttpService[IO] = HttpService[IO] {
     // html page
+    case GET -> username /: "article" /: filePath
+      if filePath.lastOption.fold(false)(_.endsWith(".json")) =>
+
+      val articleContent = {
+        val filPathStr = filePath.toString.tail //ignore '/' at beginning
+        blogtech.core.gitOps.getFile(username, filPathStr.take(filPathStr.length - 5)) //remove ".json"
+      }
+
+      Task.fromFuture(articleContent)
+        .map{
+          case Right(content) =>
+            compact(render(("result" -> 200) ~ ("data" -> content)))
+          case Left(error) =>
+            compact(render(("result" -> 400) ~ ("data" -> error.toString)))
+        }
+        .toIO
+        .flatMap(x =>
+          Ok(x).putHeaders(Header("Content-Type", "application/json"))
+        )
+
     case GET -> username /: "article" /: filePath  =>
       val articleContent = {
         val filPathStr = filePath.toString.tail //ignore '/' at beginning
