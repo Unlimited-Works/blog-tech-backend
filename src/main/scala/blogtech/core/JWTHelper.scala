61ecef537794b21891d9254655be3451a5ab533f
LoranceChen <w68055010@qq.com>
2018-03-09 23:12:17 +0800

    fix jwt parser

--- src/main/scala/blogtech/core/JWTHelper.scala
+++ src/main/scala/blogtech/core/JWTHelper.scala
@@ -22,30 +22,44 @@ object JWTHelper {
   //saved keys
   case class PayLoad(userName: String)
 
-  def contentOf[T](token: String, jValue: JObject => T): Option[T] = {
-    Try(
-      jValue(parse(verifier.verify(token).getPayload).extract[JObject])
-    ).toOption
-  }
-
-
-  def contentEntity(token: String): Option[PayLoad] = {
-    Try(
-      parse(verifier.verify(token).getPayload).extract[PayLoad]
-    ).toOption
-  }
+//  def contentOf[T](token: String, jValue: JObject => T): Option[T] = {
+//    Try {
+//      val xx = verifier
+//        .verify(token)
+//        .getClaims
+//      jValue(
+//        parse(xx)
+//          .extract[JObject])
+//    }.recover{
+//      case x =>
+//        x
+//        println(x.getStackTraceString)
+//
+//        throw x
+//    }.toOption
+//  }
+//
+//
+//  def contentEntity(token: String): Option[PayLoad] = {
+//    Try(
+//      parse(verifier.verify(token).getPayload).extract[PayLoad]
+//    ).toOption
+//  }
 
   def userName(token: String): Option[String] = {
-    contentOf(token, x => {
-      val JString(userName) = x \ "userName"
-      userName
-    })
+    Try{verifier
+      .verify(token)
+      .getClaim("userName").asString()}.toOption
+//    contentOf(token, x => {
+//      val JString(userName) = x \ "userName"
+//      userName
+//    })
   }
 
   def create(data: Map[String, String]): String = {
     val jwt = JWT.create
       .withHeader(Map("typ" -> "JWT"))
-      .withExpiresAt(new Date(System.currentTimeMillis()))
+      .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 365L))
 
     data.foreach(x => jwt.withClaim(x._1, x._2))
 
